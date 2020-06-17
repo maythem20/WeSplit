c6e290014eeb3b0193fbb443d4d7753bbe03610f
Maythem <Maythem20@gmail.com>
2020-06-17 10:34:26 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Maythem Alsodani on 6/17/20.
+//  Copyright © 2020 Maythem Alsodani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
