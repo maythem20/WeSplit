d9188218248357ce19c4c1e228012a9e60ef38e6
Maythem <Maythem20@gmail.com>
2020-06-18 18:06:51 -0400

    WeSplit - Update 01

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,10 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State private var checkamount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 0
+     let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double {
+    let peopleCount = Double(numberOfPeople + 2)
+    let theTipPercentage = Double(tipPercentages[tipPercentage])
+    let TotalAmount = Double(checkamount) ?? 0
+    let TotalAmountPlusTip = TotalAmount * ((theTipPercentage/100) + 1)
+    let amountPerPerson = TotalAmountPlusTip / peopleCount
+        
+        return amountPerPerson
     }
-}
+        
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter Total Amount", text: $checkamount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("Tip Percentage")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Amount Per Person Including Tip")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+        .navigationBarTitle("WeSplit")
+        }
+    }
+ }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
