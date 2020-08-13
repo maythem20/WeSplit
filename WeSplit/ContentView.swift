7e082c272526505b80e1e50a9edc662623eaca41
Maythem <Maythem20@gmail.com>
2020-08-12 21:10:31 -0400

    Fix issue with total amount including tip.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,8 +13,8 @@ struct ContentView: View {
     @State private var numberOfPeople = ""
     @State private var tipPercentage = 0
     @State private var TotalAmount = 0.0
-     let tipPercentages = [10,15,20,25,0]
-    
+    @State private var MinTip = false
+    let tipPercentages = [0,10,12,15,17,20]
     
     var totalPerPerson: Double {
     let peopleCount = Double(numberOfPeople) ?? 1
@@ -34,7 +34,16 @@ struct ContentView: View {
         
         return TotalAmountPlusTip
     }
-        
+    
+    var TipAmountPerPerson: Double {
+        _ = Double(numberOfPeople) ?? 1
+          let theTipPercentage = Double(tipPercentages[tipPercentage])
+          let TotalAmount = Double(checkamount) ?? 0
+          let TipAmount = TotalAmount * (theTipPercentage/100)
+          
+          return TipAmount
+    
+    }
     var body: some View {
         NavigationView {
             Form {
@@ -44,8 +53,7 @@ struct ContentView: View {
                     
                     TextField("Number of People", text: $numberOfPeople)
                         .keyboardType(.numberPad)
-                    }
-                
+                }
                 Section(header: Text("Tip Percentage")) {
                     Picker("Tip Percentage", selection: $tipPercentage) {
                         ForEach(0..<tipPercentages.count) {
@@ -54,15 +62,18 @@ struct ContentView: View {
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
+                Section(header: Text("Tip Amount Per Person")) {
+                    Text("$\(TipAmountPerPerson, specifier: "%.2f")")
+                }
                 Section(header: Text("Amount Per Person Including Tip")) {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
                 Section(header: Text("Grand Total Amount Including Tip")) {
                     Text("$\(TotalAmountIncludeTip, specifier:"%.2f")")
-                    
                 }
+                .foregroundColor(tipPercentage == 0 ? .clear : .red)
             }
-        .navigationBarTitle("WeSplit")
+        .navigationBarTitle("WeSplit 🤸🏻‍♀️")
         }
     }
 }
